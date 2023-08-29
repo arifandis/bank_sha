00c56c243da4ab7ba3f5ae86b4ae0a4370152b8f
arifandiswinata@gmail.com <arifandis@maxxitani.id>
2023-08-30 00:34:59 +0700

    refactor navigator with routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:bank_sha/ui/pages/onboarding_page.dart';
+import 'package:bank_sha/ui/pages/sign_in_page.dart';
 import 'package:bank_sha/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      routes: {
+        '/': (context) => const SplashPage(),
+        '/onboarding': (context) => const OnboardingPage(),
+        '/sign-in': (context) => const SignInPage()
+      },
     );
   }
 }
